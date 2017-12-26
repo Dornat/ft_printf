2b1f5895c0a9e4df56537f13cb065d65568514bf
Dmytro POLOSUKHIN <dpolosuk@e1r3p6.unit.ua>
2017-12-26 21:57:02 +0200

    pull

--- lftpf/ft_pf_res_d_i_precision.c
+++ lftpf/ft_pf_res_d_i_precision.c
@@ -6,7 +6,7 @@
 /*   By: dpolosuk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/22 18:02:54 by dpolosuk          #+#    #+#             */
-/*   Updated: 2017/12/25 12:39:55 by dpolosuk         ###   ########.fr       */
+/*   Updated: 2017/12/26 21:56:48 by dpolosuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,7 @@ static void		ft_pf_res_d_i_precision_while(char **res, char **s, \
 	}
 }
 
-<<<<<<< HEAD
 char			*ft_pf_res_d_i_precision(char *s, t_format *all)
-=======
-char	*ft_pf_res_d_i_precision(char *s, t_format *all)
->>>>>>> 7e0a6491fec35e2cd22c9ce63bed84838975854c
 {
 	char	*res;
 	int		len;
@@ -47,11 +43,7 @@ char	*ft_pf_res_d_i_precision(char *s, t_format *all)
 	num_of_zeroes = (*all).precision_field - len;
 	i = ft_strlen(s) + num_of_zeroes;
 	res = ft_strnew(i);
-<<<<<<< HEAD
-	--i;
-=======
 	i--;
->>>>>>> 7e0a6491fec35e2cd22c9ce63bed84838975854c
 	ft_pf_res_d_i_precision_while(&res, &s, &i, &j);
 	while (num_of_zeroes--)
 		res[i--] = '0';
